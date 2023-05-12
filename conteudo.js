9c9d79449e5f87c76a70ea229a206070463e7e45
Nicollauda ❤ <104602677+Nicolle-Oliveira@users.noreply.github.com>
2023-05-12 06:27:19 -0300

    Delete conteudo.js

--- conteudo.js
+++
@@ -1,9 +0,0 @@
-function toggleMode(){
-    const mode = document.getElementById("mode");
-
-    if(mode.className === "light-mode"){
-        mode.className = "dark-mode";
-    }else{
-        mode.className = "light-mode";
-    }
-}
